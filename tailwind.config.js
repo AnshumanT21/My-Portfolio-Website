4b3abbcd554c5f95d81bfb69a5de333f75a5ad21
AnshumanT21 <anshumantyagi20@gmail.com>
2025-12-02 17:14:33 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", /* This captures Components, components, Pages, pages, etc. */
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
